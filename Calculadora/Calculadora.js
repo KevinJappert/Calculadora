c82c28d57621c4e5c328e6b551accbc6d015af42
KevinJappert <jappertkevin@gmail.com>
2022-05-04 23:28:39 -0300

    push calculadora finalizada

--- Calculadora/Calculadora.js
+++ Calculadora/Calculadora.js
@@ -1,23 +1,17 @@
 class Calculadora {
     sumar(num1, num2) {
-        return num1 + num2
+        return num1 + num2;
     }
-}
 
-class Calculadora {
     restar(num1, num2) {
-        return num1 - num2
+        return num1 - num2;
     }
-}
 
-class Calculadora {
     dividir(num1, num2) {
-        return num1 / num2
+        return num1 / num2;
     }
-}
 
-class Calculadora {
     multiplicar(num1, num2) {
-        return num1 * num2
+        return num1 * num2;
     }
-}
+} 
